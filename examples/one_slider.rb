05d623e7f4194300be17e15a6b6201649c66002e
Michael Neumann <mneumann@ntecs.de>
2018-05-13 17:06:39 +0200

    Add one more example

---
+++ examples/one_slider.rb
@@ -0,0 +1,22 @@
+$LOAD_PATH.unshift '../lib'
+require 'eui/rect'
+require 'eui/point'
+require 'eui/lazy'
+require 'eui/ui'
+require 'eui/widgets/slider'
+
+def main
+  w = 600
+  h = 480
+  UI.new(w, h, 'immui', '/usr/local/share/fonts/dejavu/DejaVuSans.ttf', 18).run do |renderer, ui_state|
+
+    id = :slider
+	widget_state = lazy(ui_state, id)
+    style = {
+      border: ui_state[:focus] == id ? renderer.black : nil
+    }
+	slider_x(renderer, Rect.new(10, 10, w-20, h-20) , widget_state, style) { |s| ui_state[:focus] = s[:id] }
+  end
+end
+
+main if $PROGRAM_NAME == __FILE__
